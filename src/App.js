f7abebe7d49ca85ff11ec732b31f17dbb5dd3c07
Vengeance <haerulanwar24@gmail.com>
2021-06-15 22:02:36 +0700

    Kasir Web v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Row, Col, Container } from 'react-bootstrap';
+import { Category, Result, NavbarComponent, Menus } from './components';
+import { API_URL } from './utils/constants';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      menus: [],
+      kategoriDipilih: 'Makanan'
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "products?category.nama=" + this.state.kategoriDipilih)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  changeCategory = (value) => {
+    this.setState({
+      kategoriDipilih: value,
+      menus: []
+    })
+    axios
+      .get(API_URL + "products?category.nama=" + value)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  render() {
+    const { menus, kategoriDipilih } = this.state
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-3">
+          <Container fluid>
+            <Row>
+              <Category changeCategory={this.changeCategory} kategoriDipilih={kategoriDipilih}/>
+              <Col>
+                <h4><strong>List Produk</strong></h4>
+                <hr />
+                <Row>
+                  {menus && menus.map((menu) => (
+                    <Menus
+                      key={menu.id}
+                      menu={menu}
+                    />
+                  ))}
+                </Row>
+              </Col>
+              <Result />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
